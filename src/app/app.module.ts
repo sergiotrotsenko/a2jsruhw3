5e9555949261dfc4432eab8edb4fe04840ab559a
sergiotrotsenko <sergio.trotsenko@gmail.com>
2017-06-11 21:05:39 +0300

    main task done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { SuccessComponent } from './success/success.component';
+import { CustomValidator } from './common/customValidator';
+
+import { RouterModule, Routes} from '@angular/router';
+import { routes } from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [CustomValidator],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
